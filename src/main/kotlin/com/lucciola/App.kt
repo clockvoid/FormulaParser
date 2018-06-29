615d87715667786bf33b887c5f728f6cc225d0a0
Lucciola <hidenari.t1997nov@gmail.com>
2018-06-29 11:10:35 +0900

    fix: expression syntax in main function

--- src/main/kotlin/com/lucciola/App.kt
+++ src/main/kotlin/com/lucciola/App.kt
@@ -3,7 +3,6 @@ package com.lucciola
 import com.lucciola.formulaparser.Parser
 
 fun main(args: Array<String>) {
-    // TODO: it cannot calculate units
-    val parser = Parser("3 [m] * 1 [m]")
+    val parser = Parser("3[m^2] / 1[m]")
     println(parser.compile().evaluate())
 }

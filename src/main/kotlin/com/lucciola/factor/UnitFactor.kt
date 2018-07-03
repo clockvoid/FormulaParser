0d676e0e2326d2c0b63aeb4de4f5749f8ad7fca7
Lucciola <hidenari.t1997nov@gmail.com>
2018-07-03 21:42:10 +0900

    fix: factor package for unit testing

--- src/main/kotlin/com/lucciola/factor/UnitFactor.kt
+++ src/main/kotlin/com/lucciola/factor/UnitFactor.kt
@@ -1,17 +1,14 @@
 package com.lucciola.factor
 
 import com.lucciola.exception.RuntimeErrorException
-import com.lucciola.formulaparser.Expression
 import com.lucciola.termination.Unit
 
-class UnitFactor(arg0: Unit,  arg1: Unit) : Factor(arg0) {
-
-    private var outSideUnit: Expression = arg1
+class UnitFactor(arg0: Unit,  var outSideUnit: Unit) : Factor(arg0) {
 
     @Throws(RuntimeErrorException::class)
     override fun evaluate(): String {
         if (this.child.evaluate() == "void^0") {
-            return outSideUnit.evaluate()
+            return this.outSideUnit.evaluate()
         } else {
             throw RuntimeErrorException("A unit that is in brackets must be void.")
         }
